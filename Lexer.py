fa06ea89352ec09bb57b8622f3ced843ca1c97b2
Ivan <im.moralesivan@gmail.com>
2018-06-07 22:01:49 -0300

    Yay
    Felicidad :(

--- Lexer.py
+++ Lexer.py
@@ -4,3 +4,5 @@ def lexer(cadena):
     opLog = [":=", "<", ">", ">=", "<=", "!=", "=="]
     opMat = ["+", "*", "-", "/"]
     simbolos = ["(", ")", "{", "}", ",", ";"]
+
+Someday this will compile and be a working lexer... one can only dream.
