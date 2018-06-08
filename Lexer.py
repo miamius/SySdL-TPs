de18f4f3edb12f893424403961b3d96aa8eb9ea1
Jairo Laupa <jairo.laupa@gmail.com>
2018-06-07 22:36:54 -0300

    Toma papu

--- Lexer.py
+++ Lexer.py
@@ -1,16 +1,17 @@
 def lexer(cadena):
+   
     tokens = []
     reservadas = ["int", "float", "if", "for", "while"]
     opLog = [":=", "<", ">", ">=", "<=", "!=", "=="]
     opMat = ["+", "*", "-", "/"]
     simbolos = ["(", ")", "{", "}", ",", ";"]
 
-<<<<<<< HEAD
+
+    
     acum = ""
-    for caracter in cadena:
-        if caracter.isdigit():
-            acum = acum + caracter
+    for i in range(len (cadena)):	
+        print(i,cadena[i])
+        #if caracter.isdigit():
+       # acum = acum + caracter
             
-=======
-Someday this will compile and be a working lexer... one can only dream.
->>>>>>> fa06ea89352ec09bb57b8622f3ced843ca1c97b2
+lexer('hola');
